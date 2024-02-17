1d38ccd98fed05f4f2cada351c227df3b868ba97
Damir Akhmetgaleev <artbox12345@mail.ru>
2024-02-17 23:23:11 +0500

    save16

---
+++ PYTHON_COURSE_1/DZseminar06.py
@@ -0,0 +1,21 @@
+# Задача 32:
+# Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону
+# (т.е. не меньше заданного минимума и не больше заданного максимума)
+# РЕШЕНИЕ
+# list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+# min_number = 0
+# max_number = 10
+# indices = []
+# for i in range(len(list_1)):
+#     if min_number < list_1[i] < max_number:
+#         indices.append(i)
+# print("Индексы элементов, значения которых принадлежат диапазону от", min_number, "до", max_number, ":", indices)
+
+# или так
+
+# list=[-5,9,0,3,-1,-2,1,4,-2,10,2,0,-9,8,10,-9,0,-5,-5,7]
+# min_number = int(input())
+# max_number = int(input())
+# for i in range(len(list)):
+#     if min_number <= list[i] <= max_number:
+#         print(i)
