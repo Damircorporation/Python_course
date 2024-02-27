bcacef2434c2bdf64c6424f42a5f28bc90f4384a
Damir Akhmetgaleev <artbox12345@mail.ru>
2024-02-27 21:30:15 +0500

    save 15

--- PYTHON_COURSE_1/DZseminar07.py
+++ PYTHON_COURSE_1/DZseminar07.py
@@ -19,22 +19,22 @@
 # else: res = "Пам парам"
 # print(res)
 
-# Получаем стихотворение от пользователя
-stroka = input("Введите стихотворение Винни-Пуха: ")
-# Разбиваем стихотворение на фразы
-phrases = stroka.split()
-# for i in phrases:
-#     if phrases[i] == 1:
-#          print("Количество фраз должно быть больше одной!")
-# Функция для подсчета количества гласных букв в фразе
-count_vowels = lambda phrase: sum(1 for letter in phrase if letter.lower() in 'aeiouаеёиоуыэюя')
-# Подсчитываем количество гласных в каждой фразе
-syllable_counts = [count_vowels(phrase) for phrase in phrases]
-# Проверяем, одинаково ли количество гласных во всех фразах
-if all(count == syllable_counts[0] for count in syllable_counts):
-    print("Парам пам-пам")
-else:
-    print("Пам парам")
+# # Получаем стихотворение от пользователя
+# stroka = input("Введите стихотворение Винни-Пуха: ")
+# # Разбиваем стихотворение на фразы
+# phrases = stroka.split()
+# # for i in phrases:
+# #     if phrases[i] == 1:
+# #          print("Количество фраз должно быть больше одной!")
+# # Функция для подсчета количества гласных букв в фразе
+# count_vowels = lambda phrase: sum(1 for letter in phrase if letter.lower() in 'aeiouаеёиоуыэюя')
+# # Подсчитываем количество гласных в каждой фразе
+# syllable_counts = [count_vowels(phrase) for phrase in phrases]
+# # Проверяем, одинаково ли количество гласных во всех фразах
+# if all(count == syllable_counts[0] for count in syllable_counts):
+#     print("Парам пам-пам")
+# else:
+#     print("Пам парам")
 
 # Задача №2 Напишите функцию print_operation_table(operation, num_rows, num_columns), 
 # которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и столбца. 
